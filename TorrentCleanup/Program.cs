8981d94c5b69bfb293161c709896aa7fbd7c3004
Mike <mikeundead@googlemail.com>
2018-06-13 19:11:42 +0200

    dll

--- TorrentCleanup/Program.cs
+++ TorrentCleanup/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Transmission.API.RPC;
-using Transmission.API.RPC.Entity;
+﻿using TorrentCleanupDLL;
 
 namespace TorrentCleanup
 {
@@ -10,30 +6,8 @@ namespace TorrentCleanup
     {
         public static void Main(string[] args)
         {
-            string ip = args.Length == 0 ? "127.0.0.1" : args[0];
-
-            string host = $"http://{ip}:9091/transmission/rpc";
-
-            Client client = new Client(host);
-
-            var torrentsInfo = client.TorrentGet(TorrentFields.ALL_FIELDS);
-            var torrentsToRemove = new List<int>();
-
-            foreach (var torrent in torrentsInfo.Torrents)
-            {
-                var stats = torrent.Files.ToList();
-                var totalDownload = stats.Sum(x => x.BytesCompleted);
-                var totalSize = stats.Sum(x => x.Length);
-
-                if (totalDownload == totalSize)
-                {
-                    torrentsToRemove.Add(torrent.ID);
-
-                    Console.WriteLine($"Torrent added: {torrent.Name}");
-                }
-            }
-
-            client.TorrentRemove(torrentsToRemove.ToArray());
+            Cleanup cleanup = new Cleanup();
+            cleanup.Clean();
         }
     }
 }
---
+++ TorrentCleanupDLL/Cleanup.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Transmission.API.RPC;
+using Transmission.API.RPC.Entity;
+
+namespace TorrentCleanupDLL
+{
+    public class Cleanup
+    {
+        public void Clean()
+        {
+            string host = $"http://127.0.0.1:9091/transmission/rpc";
+
+            Client client = new Client(host);
+
+            var torrentsInfo = client.TorrentGet(TorrentFields.ALL_FIELDS);
+            var torrentsToRemove = new List<int>();
+
+            foreach (var torrent in torrentsInfo.Torrents)
+            {
+                var stats = torrent.Files.ToList();
+                var totalDownload = stats.Sum(x => x.BytesCompleted);
+                var totalSize = stats.Sum(x => x.Length);
+
+                if (totalDownload == totalSize)
+                {
+                    torrentsToRemove.Add(torrent.ID);
+                }
+            }
+
+            client.TorrentRemove(torrentsToRemove.ToArray());
+        }
+    }
+}
